abe0e4947a693057d138bbf141140f2365e62343
Levan <levan.soroznishvili@gmail.com>
2024-03-19 23:18:31 +0400

    inventory management login

---
+++ InventoryManagementSystem/InventoryManagementSystem/Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TabControl1 = New TabControl()
+        TabPage1 = New TabPage()
+        TabPage2 = New TabPage()
+        TabPage3 = New TabPage()
+        GroupBox1 = New GroupBox()
+        btnExit = New Button()
+        btnReset = New Button()
+        btnPassword = New Button()
+        txtPassword = New TextBox()
+        txtUsername = New TextBox()
+        Label3 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        TabControl1.SuspendLayout()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' TabControl1
+        ' 
+        TabControl1.Controls.Add(TabPage1)
+        TabControl1.Controls.Add(TabPage2)
+        TabControl1.Controls.Add(TabPage3)
+        TabControl1.Location = New Point(584, 12)
+        TabControl1.Name = "TabControl1"
+        TabControl1.SelectedIndex = 0
+        TabControl1.Size = New Size(671, 362)
+        TabControl1.TabIndex = 0
+        ' 
+        ' TabPage1
+        ' 
+        TabPage1.BackColor = SystemColors.Control
+        TabPage1.Location = New Point(4, 24)
+        TabPage1.Name = "TabPage1"
+        TabPage1.Padding = New Padding(3)
+        TabPage1.Size = New Size(663, 334)
+        TabPage1.TabIndex = 0
+        TabPage1.Text = "Customer Details"
+        ' 
+        ' TabPage2
+        ' 
+        TabPage2.BackColor = SystemColors.Control
+        TabPage2.Location = New Point(4, 24)
+        TabPage2.Name = "TabPage2"
+        TabPage2.Padding = New Padding(3)
+        TabPage2.Size = New Size(663, 334)
+        TabPage2.TabIndex = 1
+        TabPage2.Text = "Default Settings"
+        ' 
+        ' TabPage3
+        ' 
+        TabPage3.BackColor = SystemColors.Control
+        TabPage3.Location = New Point(4, 24)
+        TabPage3.Name = "TabPage3"
+        TabPage3.Padding = New Padding(3)
+        TabPage3.Size = New Size(663, 334)
+        TabPage3.TabIndex = 2
+        TabPage3.Text = "Credit Control"
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(btnExit)
+        GroupBox1.Controls.Add(btnReset)
+        GroupBox1.Controls.Add(btnPassword)
+        GroupBox1.Controls.Add(txtPassword)
+        GroupBox1.Controls.Add(txtUsername)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Location = New Point(12, 12)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(384, 224)
+        GroupBox1.TabIndex = 1
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "GroupBox1"
+        ' 
+        ' btnExit
+        ' 
+        btnExit.Location = New Point(227, 156)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(75, 23)
+        btnExit.TabIndex = 7
+        btnExit.Text = "Exit"
+        btnExit.UseVisualStyleBackColor = True
+        ' 
+        ' btnReset
+        ' 
+        btnReset.Location = New Point(146, 156)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(75, 23)
+        btnReset.TabIndex = 6
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
+        ' 
+        ' btnPassword
+        ' 
+        btnPassword.Location = New Point(65, 156)
+        btnPassword.Name = "btnPassword"
+        btnPassword.Size = New Size(75, 23)
+        btnPassword.TabIndex = 5
+        btnPassword.Text = "Password"
+        btnPassword.UseVisualStyleBackColor = True
+        ' 
+        ' txtPassword
+        ' 
+        txtPassword.Location = New Point(128, 115)
+        txtPassword.Name = "txtPassword"
+        txtPassword.Size = New Size(174, 23)
+        txtPassword.TabIndex = 4
+        ' 
+        ' txtUsername
+        ' 
+        txtUsername.Location = New Point(128, 83)
+        txtUsername.Name = "txtUsername"
+        txtUsername.Size = New Size(174, 23)
+        txtUsername.TabIndex = 3
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(65, 118)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(57, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Password"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(62, 86)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(60, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Username"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 18F)
+        Label1.Location = New Point(17, 24)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(348, 32)
+        Label1.TabIndex = 0
+        Label1.Text = "Inventory Management System"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1352, 661)
+        Controls.Add(GroupBox1)
+        Controls.Add(TabControl1)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        TabControl1.ResumeLayout(False)
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnPassword As Button
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ InventoryManagementSystem/InventoryManagementSystem/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub btnPassword_Click(sender As Object, e As EventArgs) Handles btnPassword.Click
+        If txtUsername.Text = "admin" And txtPassword.Text = "111111" Then
+            GroupBox1.Visible = False
+        Else
+            MessageBox.Show("You have entered an incorrect login details", "Closing Exams", MessageBoxButtons.OK, MessageBoxIcon.Question)
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        txtPassword.Clear()
+        txtUsername.Clear()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Dim result = MessageBox.Show("Are you shure you would like to exit?", "Closing Exams", MessageBoxButtons.OK, MessageBoxIcon.Question)
+
+        If (result = DialogResult.Yes) Then
+            Application.Exit()
+        End If
+    End Sub
+End Class
