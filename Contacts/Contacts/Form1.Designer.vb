fe485efe162a42fa5037909b30ea97ba22b350ef
Levan <levan.soroznishvili@gmail.com>
2024-03-27 23:50:35 +0400

    search completed

--- Contacts/Contacts/Form1.Designer.vb
+++ Contacts/Contacts/Form1.Designer.vb
@@ -33,26 +33,28 @@ Partial Class Form1
         Me.lbl_birthDate = New System.Windows.Forms.Label()
         Me.zodiac = New System.Windows.Forms.ComboBox()
         Me.lbl_zodiac = New System.Windows.Forms.Label()
-        Me.btn_update = New System.Windows.Forms.Button()
+        Me.btn_edit = New System.Windows.Forms.Button()
         Me.btn_delete = New System.Windows.Forms.Button()
         Me.btn_clear = New System.Windows.Forms.Button()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.search = New System.Windows.Forms.TextBox()
-        Me.lbl_search = New System.Windows.Forms.Label()
         Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.search = New System.Windows.Forms.TextBox()
+        Me.lbl_search = New System.Windows.Forms.Label()
         Me.id = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.btn_refresh = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'lbl_firstName
         '
         Me.lbl_firstName.AutoSize = True
-        Me.lbl_firstName.Location = New System.Drawing.Point(9, 72)
+        Me.lbl_firstName.Location = New System.Drawing.Point(9, 53)
         Me.lbl_firstName.Name = "lbl_firstName"
         Me.lbl_firstName.Size = New System.Drawing.Size(57, 13)
         Me.lbl_firstName.TabIndex = 0
@@ -61,7 +63,7 @@ Partial Class Form1
         'lbl_lastName
         '
         Me.lbl_lastName.AutoSize = True
-        Me.lbl_lastName.Location = New System.Drawing.Point(9, 114)
+        Me.lbl_lastName.Location = New System.Drawing.Point(9, 95)
         Me.lbl_lastName.Name = "lbl_lastName"
         Me.lbl_lastName.Size = New System.Drawing.Size(58, 13)
         Me.lbl_lastName.TabIndex = 1
@@ -70,7 +72,7 @@ Partial Class Form1
         'lbl_email
         '
         Me.lbl_email.AutoSize = True
-        Me.lbl_email.Location = New System.Drawing.Point(12, 158)
+        Me.lbl_email.Location = New System.Drawing.Point(12, 139)
         Me.lbl_email.Name = "lbl_email"
         Me.lbl_email.Size = New System.Drawing.Size(36, 13)
         Me.lbl_email.TabIndex = 2
@@ -78,28 +80,28 @@ Partial Class Form1
         '
         'firstName
         '
-        Me.firstName.Location = New System.Drawing.Point(12, 88)
+        Me.firstName.Location = New System.Drawing.Point(12, 69)
         Me.firstName.Name = "firstName"
         Me.firstName.Size = New System.Drawing.Size(192, 20)
         Me.firstName.TabIndex = 1
         '
         'lastName
         '
-        Me.lastName.Location = New System.Drawing.Point(12, 130)
+        Me.lastName.Location = New System.Drawing.Point(12, 111)
         Me.lastName.Name = "lastName"
         Me.lastName.Size = New System.Drawing.Size(192, 20)
         Me.lastName.TabIndex = 2
         '
         'email
         '
-        Me.email.Location = New System.Drawing.Point(12, 174)
+        Me.email.Location = New System.Drawing.Point(12, 155)
         Me.email.Name = "email"
         Me.email.Size = New System.Drawing.Size(192, 20)
         Me.email.TabIndex = 3
         '
         'btn_save
         '
-        Me.btn_save.Location = New System.Drawing.Point(12, 301)
+        Me.btn_save.Location = New System.Drawing.Point(12, 282)
         Me.btn_save.Name = "btn_save"
         Me.btn_save.Size = New System.Drawing.Size(60, 23)
         Me.btn_save.TabIndex = 21
@@ -108,7 +110,7 @@ Partial Class Form1
         '
         'birthDate
         '
-        Me.birthDate.Location = New System.Drawing.Point(12, 218)
+        Me.birthDate.Location = New System.Drawing.Point(12, 199)
         Me.birthDate.Name = "birthDate"
         Me.birthDate.Size = New System.Drawing.Size(192, 20)
         Me.birthDate.TabIndex = 4
@@ -116,7 +118,7 @@ Partial Class Form1
         'lbl_birthDate
         '
         Me.lbl_birthDate.AutoSize = True
-        Me.lbl_birthDate.Location = New System.Drawing.Point(9, 202)
+        Me.lbl_birthDate.Location = New System.Drawing.Point(9, 183)
         Me.lbl_birthDate.Name = "lbl_birthDate"
         Me.lbl_birthDate.Size = New System.Drawing.Size(54, 13)
         Me.lbl_birthDate.TabIndex = 8
@@ -126,7 +128,7 @@ Partial Class Form1
         '
         Me.zodiac.FormattingEnabled = True
         Me.zodiac.Items.AddRange(New Object() {"Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"})
-        Me.zodiac.Location = New System.Drawing.Point(12, 263)
+        Me.zodiac.Location = New System.Drawing.Point(12, 244)
         Me.zodiac.Name = "zodiac"
         Me.zodiac.Size = New System.Drawing.Size(192, 21)
         Me.zodiac.TabIndex = 5
@@ -134,24 +136,24 @@ Partial Class Form1
         'lbl_zodiac
         '
         Me.lbl_zodiac.AutoSize = True
-        Me.lbl_zodiac.Location = New System.Drawing.Point(13, 247)
+        Me.lbl_zodiac.Location = New System.Drawing.Point(13, 228)
         Me.lbl_zodiac.Name = "lbl_zodiac"
         Me.lbl_zodiac.Size = New System.Drawing.Size(40, 13)
         Me.lbl_zodiac.TabIndex = 10
         Me.lbl_zodiac.Text = "Zodiac"
         '
-        'btn_update
+        'btn_edit
         '
-        Me.btn_update.Location = New System.Drawing.Point(78, 301)
-        Me.btn_update.Name = "btn_update"
-        Me.btn_update.Size = New System.Drawing.Size(60, 23)
-        Me.btn_update.TabIndex = 22
-        Me.btn_update.Text = "Update"
-        Me.btn_update.UseVisualStyleBackColor = True
+        Me.btn_edit.Location = New System.Drawing.Point(78, 282)
+        Me.btn_edit.Name = "btn_edit"
+        Me.btn_edit.Size = New System.Drawing.Size(60, 23)
+        Me.btn_edit.TabIndex = 22
+        Me.btn_edit.Text = "Update"
+        Me.btn_edit.UseVisualStyleBackColor = True
         '
         'btn_delete
         '
-        Me.btn_delete.Location = New System.Drawing.Point(144, 301)
+        Me.btn_delete.Location = New System.Drawing.Point(144, 282)
         Me.btn_delete.Name = "btn_delete"
         Me.btn_delete.Size = New System.Drawing.Size(60, 23)
         Me.btn_delete.TabIndex = 23
@@ -160,7 +162,7 @@ Partial Class Form1
         '
         'btn_clear
         '
-        Me.btn_clear.Location = New System.Drawing.Point(12, 330)
+        Me.btn_clear.Location = New System.Drawing.Point(12, 311)
         Me.btn_clear.Name = "btn_clear"
         Me.btn_clear.Size = New System.Drawing.Size(60, 23)
         Me.btn_clear.TabIndex = 24
@@ -170,17 +172,59 @@ Partial Class Form1
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5})
-        Me.DataGridView1.Location = New System.Drawing.Point(223, 86)
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6})
+        Me.DataGridView1.Location = New System.Drawing.Point(223, 67)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowHeadersVisible = False
         Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
         Me.DataGridView1.Size = New System.Drawing.Size(349, 263)
         Me.DataGridView1.TabIndex = 25
         '
+        'Column1
+        '
+        Me.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.Column1.HeaderText = "ID"
+        Me.Column1.Name = "Column1"
+        Me.Column1.Width = 43
+        '
+        'Column2
+        '
+        Me.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.Column2.HeaderText = "First Name"
+        Me.Column2.Name = "Column2"
+        Me.Column2.Width = 82
+        '
+        'Column3
+        '
+        Me.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.Column3.HeaderText = "Last Name"
+        Me.Column3.Name = "Column3"
+        Me.Column3.Width = 83
+        '
+        'Column4
+        '
+        Me.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.Column4.HeaderText = "E-Mail"
+        Me.Column4.Name = "Column4"
+        Me.Column4.Width = 61
+        '
+        'Column5
+        '
+        Me.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.Column5.HeaderText = "Birth Date"
+        Me.Column5.Name = "Column5"
+        Me.Column5.Width = 79
+        '
+        'Column6
+        '
+        Me.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.Column6.HeaderText = "Zodiac"
+        Me.Column6.Name = "Column6"
+        Me.Column6.Width = 65
+        '
         'search
         '
-        Me.search.Location = New System.Drawing.Point(223, 44)
+        Me.search.Location = New System.Drawing.Point(223, 25)
         Me.search.Name = "search"
         Me.search.Size = New System.Drawing.Size(349, 20)
         Me.search.TabIndex = 27
@@ -188,50 +232,15 @@ Partial Class Form1
         'lbl_search
         '
         Me.lbl_search.AutoSize = True
-        Me.lbl_search.Location = New System.Drawing.Point(220, 28)
+        Me.lbl_search.Location = New System.Drawing.Point(220, 9)
         Me.lbl_search.Name = "lbl_search"
         Me.lbl_search.Size = New System.Drawing.Size(41, 13)
         Me.lbl_search.TabIndex = 26
         Me.lbl_search.Text = "Search"
         '
-        'Column1
-        '
-        Me.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
-        Me.Column1.HeaderText = "First Name"
-        Me.Column1.Name = "Column1"
-        Me.Column1.Width = 82
-        '
-        'Column2
-        '
-        Me.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
-        Me.Column2.HeaderText = "Last Name"
-        Me.Column2.Name = "Column2"
-        Me.Column2.Width = 83
-        '
-        'Column3
-        '
-        Me.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
-        Me.Column3.HeaderText = "E-Mail"
-        Me.Column3.Name = "Column3"
-        Me.Column3.Width = 61
-        '
-        'Column4
-        '
-        Me.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
-        Me.Column4.HeaderText = "Birth Date"
-        Me.Column4.Name = "Column4"
-        Me.Column4.Width = 79
-        '
-        'Column5
-        '
-        Me.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
-        Me.Column5.HeaderText = "Zodiac"
-        Me.Column5.Name = "Column5"
-        Me.Column5.Width = 65
-        '
         'id
         '
-        Me.id.Location = New System.Drawing.Point(12, 44)
+        Me.id.Location = New System.Drawing.Point(12, 25)
         Me.id.Name = "id"
         Me.id.Size = New System.Drawing.Size(192, 20)
         Me.id.TabIndex = 29
@@ -239,17 +248,27 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(9, 28)
+        Me.Label1.Location = New System.Drawing.Point(9, 9)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(18, 13)
         Me.Label1.TabIndex = 28
         Me.Label1.Text = "ID"
+        '
+        'btn_refresh
+        '
+        Me.btn_refresh.Location = New System.Drawing.Point(78, 311)
+        Me.btn_refresh.Name = "btn_refresh"
+        Me.btn_refresh.Size = New System.Drawing.Size(60, 23)
+        Me.btn_refresh.TabIndex = 30
+        Me.btn_refresh.Text = "Refresh"
+        Me.btn_refresh.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(584, 361)
+        Me.Controls.Add(Me.btn_refresh)
         Me.Controls.Add(Me.id)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.search)
@@ -257,7 +276,7 @@ Partial Class Form1
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.btn_clear)
         Me.Controls.Add(Me.btn_delete)
-        Me.Controls.Add(Me.btn_update)
+        Me.Controls.Add(Me.btn_edit)
         Me.Controls.Add(Me.lbl_zodiac)
         Me.Controls.Add(Me.zodiac)
         Me.Controls.Add(Me.lbl_birthDate)
@@ -289,17 +308,19 @@ Partial Class Form1
     Friend WithEvents lbl_birthDate As Label
     Friend WithEvents zodiac As ComboBox
     Friend WithEvents lbl_zodiac As Label
-    Friend WithEvents btn_update As Button
+    Friend WithEvents btn_edit As Button
     Friend WithEvents btn_delete As Button
     Friend WithEvents btn_clear As Button
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents search As TextBox
     Friend WithEvents lbl_search As Label
+    Friend WithEvents id As TextBox
+    Friend WithEvents Label1 As Label
     Friend WithEvents Column1 As DataGridViewTextBoxColumn
     Friend WithEvents Column2 As DataGridViewTextBoxColumn
     Friend WithEvents Column3 As DataGridViewTextBoxColumn
     Friend WithEvents Column4 As DataGridViewTextBoxColumn
     Friend WithEvents Column5 As DataGridViewTextBoxColumn
-    Friend WithEvents id As TextBox
-    Friend WithEvents Label1 As Label
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents btn_refresh As Button
 End Class
--- Contacts/Contacts/Form1.vb
+++ Contacts/Contacts/Form1.vb
@@ -11,11 +11,11 @@ Public Class Form1
     Public Sub DGV_load()
         DataGridView1.Rows.Clear()
         Try
-        conn.Open()
+            conn.Open()
             Dim cmd As New MySqlCommand("SELECT * FROM `contacts`", conn)
             dr = cmd.ExecuteReader
             While dr.Read
-                DataGridView1.Rows.Add(dr.Item("first_name"), dr.Item("last_name"), dr.Item("email"), dr.Item("birth_date"), dr.Item("zodiac"))
+                DataGridView1.Rows.Add(dr.Item("id"), dr.Item("first_name"), dr.Item("last_name"), dr.Item("email"), dr.Item("birth_date"), dr.Item("zodiac"))
             End While
             dr.Dispose()
         Catch ex As Exception
@@ -26,10 +26,11 @@ Public Class Form1
     End Sub
 
     Private Sub btn_save_Click(sender As Object, e As EventArgs) Handles btn_save.Click
-        save()
+        Save()
+        DGV_load()
     End Sub
 
-    Public Sub save()
+    Public Sub Save()
         Try
             conn.Open()
             Dim cmd As New MySqlCommand("INSERT INTO `contacts`(`first_name`, `last_name`, `email`, `birth_date`, `zodiac`) VALUES(@first_name,@last_name,@email,@birth_date,@zodiac)", conn)
@@ -59,42 +60,43 @@ Public Class Form1
     End Sub
 
     Public Sub clear()
+        id.Clear()
         firstName.Clear()
         lastName.Clear()
         email.Clear()
-        birthDate.Text = ""
-        zodiac.Text = Now()
+        birthDate.Value = Now
+        zodiac.Text = ""
     End Sub
 
     Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
-        firstName.Text = DataGridView1.CurrentRow.Cells(0).Value
-        lastName.Text = DataGridView1.CurrentRow.Cells(1).Value
-        email.Text = DataGridView1.CurrentRow.Cells(2).Value
-        birthDate.Text = DataGridView1.CurrentRow.Cells(3).Value
-        zodiac.Text = DataGridView1.CurrentRow.Cells(4).Value
+        id.Text = DataGridView1.CurrentRow.Cells(0).Value
+        firstName.Text = DataGridView1.CurrentRow.Cells(1).Value
+        lastName.Text = DataGridView1.CurrentRow.Cells(2).Value
+        email.Text = DataGridView1.CurrentRow.Cells(3).Value
+        birthDate.Text = DataGridView1.CurrentRow.Cells(4).Value
+        zodiac.Text = DataGridView1.CurrentRow.Cells(5).Value
 
+        id.ReadOnly = True
         btn_save.Enabled = False
     End Sub
 
     Sub Edit()
         Try
             conn.Open()
-            Dim cmd As New MySqlCommand("UPDATE `contacts` SET `first_name`=@firstName,`last_name`=@lastName,`email`=@email,`birth_date`=@birthDate,`zodiac`=@zodiac WHERE `zodiac`=@id", conn)
-            cmd.Parameters.Clear()
+            Dim cmd As New MySqlCommand("UPDATE `contacts` SET `first_name`=@first_name, `last_name`=@last_name, `email`=@email, `birth_date`=@birth_date, `zodiac`=@zodiac WHERE `id`=@id", conn)
+            cmd.Parameters.AddWithValue("@id", id.Text)
             cmd.Parameters.AddWithValue("@first_name", firstName.Text)
             cmd.Parameters.AddWithValue("@last_name", lastName.Text)
             cmd.Parameters.AddWithValue("@email", email.Text)
             cmd.Parameters.AddWithValue("@birth_date", CDate(birthDate.Value))
             cmd.Parameters.AddWithValue("@zodiac", zodiac.Text)
 
-
-
-            i = cmd.ExecuteNonQuery
+            i = cmd.ExecuteNonQuery()
 
             If i > 0 Then
                 MessageBox.Show("Record updated", "Contacts", MessageBoxButtons.OK, MessageBoxIcon.Information)
             Else
-                MessageBox.Show("Update failed", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+                MessageBox.Show("Update failed", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error)
             End If
 
         Catch ex As Exception
@@ -102,11 +104,74 @@ Public Class Form1
         Finally
             conn.Close()
         End Try
-
-        clear()
     End Sub
 
-    Private Sub btn_update_Click(sender As Object, e As EventArgs) Handles btn_update.Click
 
+    Private Sub btn_edit_Click(sender As Object, e As EventArgs) Handles btn_edit.Click
+        Edit()
+        DGV_load()
+    End Sub
+
+    Public Sub Delete()
+        If MsgBox("Are you sure delete this record", MsgBoxStyle.Question + vbYesNo) = vbYes Then
+            Try
+                conn.Open()
+                Dim cmd As New MySqlCommand("DELETE FROM `contacts` WHERE `id`=@id", conn)
+                cmd.Parameters.Clear()
+                cmd.Parameters.AddWithValue("@id", id.Text)
+
+                i = cmd.ExecuteNonQuery
+
+                If i > 0 Then
+                    MessageBox.Show("Record deleted", "Contacts", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Else
+                    MessageBox.Show("Delete failed", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+                End If
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            Finally
+                conn.Close()
+            End Try
+
+            clear()
+            DGV_load()
+        Else
+            Return
+        End If
+    End Sub
+
+    Private Sub btn_delete_Click(sender As Object, e As EventArgs) Handles btn_delete.Click
+        Delete()
+        DGV_load()
+    End Sub
+
+    Private Sub btn_clear_Click(sender As Object, e As EventArgs) Handles btn_clear.Click
+        clear()
+        DGV_load()
+    End Sub
+
+    Private Sub search_TextChanged(sender As Object, e As EventArgs) Handles search.TextChanged
+        DataGridView1.Rows.Clear()
+        Try
+            conn.Open()
+            Dim cmd As New MySqlCommand("SELECT * FROM `contacts` WHERE first_name LIKE '%" & search.Text & "%' OR last_name LIKE '%" & search.Text & "%' OR email LIKE '%" & search.Text & "%'", conn)
+
+            dr = cmd.ExecuteReader
+            While dr.Read
+                DataGridView1.Rows.Add(dr.Item("first_name"), dr.Item("last_name"), dr.Item("email"), dr.Item("birth_date"), dr.Item("zodiac"))
+            End While
+            dr.Dispose()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub btn_refresh_Click(sender As Object, e As EventArgs) Handles btn_refresh.Click
+        id.ReadOnly = False
+        btn_save.Enabled = True
+        DGV_load()
     End Sub
 End Class
